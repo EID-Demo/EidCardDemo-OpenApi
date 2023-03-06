799b403fefeed6aeaf076be30dda1a5b09eadaa9
lishaowen <shaowen.li@sunmi.com>
2023-03-06 15:57:05 +0800

    feat: update version 1.0.10

--- app/src/main/java/com/sunmi/readidcardemo/IApp.java
+++ app/src/main/java/com/sunmi/readidcardemo/IApp.java
@@ -17,19 +17,19 @@ public class IApp extends Application {
     public void onCreate() {
         super.onCreate();
         //Step 1 初始化SDK 传入AppId
-        EidSDK.init(this, Constant.APP_ID, new EidCall() {
-            @Override
-            public void onCallData(int code, String msg) {
-                switch (code) {
-                    case EidConstants.EID_INIT_SUCCESS:
-                        //初始化成功
-                        break;
-                    default:
-                        //初始化失败，只有参数错误时会失败，回调其他code请检查传入参数。
-                        break;
-                }
-            }
-        });
+//        EidSDK.init(this, Constant.APP_ID, new EidCall() {
+//            @Override
+//            public void onCallData(int code, String msg) {
+//                switch (code) {
+//                    case EidConstants.EID_INIT_SUCCESS:
+//                        //初始化成功
+//                        break;
+//                    default:
+//                        //初始化失败，只有参数错误时会失败，回调其他code请检查传入参数。
+//                        break;
+//                }
+//            }
+//        });
     }
 
 }
